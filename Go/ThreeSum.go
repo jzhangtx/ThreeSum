5f7ac25a458728f01a1d1531a7aa702f982668fa
Jing Zhang <jzhang_fl@outlook.com>
2024-12-19 13:30:49 -0600

    Commented the code the current solution does not use

--- Go/ThreeSum.go
+++ Go/ThreeSum.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"time"
+	//	"time"
 )
 
 func CountInArray(a []int, n int) int {
@@ -41,56 +41,56 @@ func Find(vv *[][]int, v *[]int) bool {
 	return false
 }
 
-func findThreeSum(nums []int, start int, sum int, a []int, result *[][]int) {
-	if sum == 0 && len(a) == 3 {
-		if !Find(result, &a) {
-			temp := make([]int, len(a))
-			copy(temp, a)
-			*result = append(*result, temp)
-		}
-		return
-	}
+// func findThreeSum(nums []int, start int, sum int, a []int, result *[][]int) {
+// 	if sum == 0 && len(a) == 3 {
+// 		if !Find(result, &a) {
+// 			temp := make([]int, len(a))
+// 			copy(temp, a)
+// 			*result = append(*result, temp)
+// 		}
+// 		return
+// 	}
 
-	if start == len(nums) || len(a) == 3 {
-		return
-	}
+// 	if start == len(nums) || len(a) == 3 {
+// 		return
+// 	}
 
-	for i := start; i < len(nums); i++ {
-		a = append(a, nums[i])
-		findThreeSum(nums, i+1, sum-nums[i], a, result)
-		a = a[:len(a)-1]
-	}
-}
+// 	for i := start; i < len(nums); i++ {
+// 		a = append(a, nums[i])
+// 		findThreeSum(nums, i+1, sum-nums[i], a, result)
+// 		a = a[:len(a)-1]
+// 	}
+// }
 
 func threeSum(nums []int) [][]int {
 	var result [][]int
-	// for i := 0; i < len(nums)-2; i++ {
-	// 	for j := i + 1; j < len(nums)-1; j++ {
-	// 		for k := j + 1; k < len(nums); k++ {
-	// 			if nums[i]+nums[j]+nums[k] == 0 {
-	// 				v := []int{nums[i], nums[j], nums[k]}
-	// 				if !Find(&result, &v) {
-	// 					result = append(result, v)
-	// 				}
-	// 			}
-	// 		}
-	// 	}
-	// }
-	var a []int
-	findThreeSum(nums, 0, 0, a, &result)
+	for i := 0; i < len(nums)-2; i++ {
+		for j := i + 1; j < len(nums)-1; j++ {
+			for k := j + 1; k < len(nums); k++ {
+				if nums[i]+nums[j]+nums[k] == 0 {
+					v := []int{nums[i], nums[j], nums[k]}
+					if !Find(&result, &v) {
+						result = append(result, v)
+					}
+				}
+			}
+		}
+	}
+	// var a []int
+	// findThreeSum(nums, 0, 0, a, &result)
 
 	return result
 }
 
 func main() {
 	v := []int{1, 1, 0, -1, -2, -3, 2, 3, -1}
-	t := time.Now()
-	for i := 0; i < 100000; i++ {
-		threeSum(v)
-	}
+	// t := time.Now()
+	// for i := 0; i < 100000; i++ {
+	// 	threeSum(v)
+	// }
 
-	d := time.Now().Sub(t)
-	fmt.Println(d)
+	// d := time.Since(t)
+	// fmt.Println(d)
 
 	r := threeSum(v)
 
